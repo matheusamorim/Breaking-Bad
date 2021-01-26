126365ba9ad35afc96ab12c8e76839ac0c4180a7
Matheus Feitosa <18matheusamorim@gmail.com>
2021-01-26 10:12:56 -0300

    update 26/01

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,53 @@
 import styled from 'styled-components'
-import db from './db.json'
-import Widget from '../componentes/widget'
-import Footer from '../componentes/Footer'
+import db from './db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-
-
-const BackgroundImage = styled.div`
-background-image: url(${db.bg});
-flex: 1;
-background-size: cover;
-background-position: center;
-`;
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
 export const QuizContainer = styled.div`
-width: 100%;
-max-width: 350px;
-padding-top: 45px;
-margin: auto 10%;
-@media screen and (max-width: 500px){
-  margin: auto;
-  padding: 15px;
-}
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `;
-
-
-
-
-
 
 export default function Home() {
   return (
-  <BackgroundImage>
-        <QuizContainer>
-          <Widget>
-              <h1> Breaking Bad </h1>
-              <p> A melhor série </p>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
 
-          </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
 
-          <Widget>
-             <h1> Breaking Bad </h1>
-              <p> A melhor série </p>
-
-          </Widget>
-        </QuizContainer>
-  </BackgroundImage>
-
-  )
-
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/matheusamorim" />
+    </QuizBackground>
+  );
 }
