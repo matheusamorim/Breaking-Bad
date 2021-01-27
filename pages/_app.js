7d552cc7eac7542c844c8107f26739ad336033d9
Matheus Feitosa <18matheusamorim@gmail.com>
2021-01-27 10:18:29 -0300

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import db from './db.json'
+import Head from 'next/head';
+
+
 
 
 const GlobalStyle = createGlobalStyle`
@@ -32,7 +35,10 @@ const theme = db.theme;
 export default function App({ Component, pageProps }) {
   return (
     <>
-      
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@200&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
       <GlobalStyle />
         <Component {...pageProps} />
